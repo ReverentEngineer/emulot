158bd7ecd76d7a0db2b46df5eeffabbe46a484bb
Jeffrey Caffrey-Hill <jeff@caffreyhill.com>
2023-03-19 08:07:31 -0400

    feat: Add 'client list' command

--- src/client/mod.rs
+++ src/client/mod.rs
@@ -13,3 +13,11 @@ pub async fn stop(config: ClientConfig, guest: String) -> Result<(), Error> {
     config.builder()?.endpoint(format!("/guests/stop/{guest}"))?.post(None)?;
     Ok(())
 }
+
+pub async fn list(config: ClientConfig) -> Result<(), Error> {
+    let guests: Vec<String> = config.builder()?.endpoint(format!("/guests/list"))?.get()?;
+    for guest in guests {
+        println!("{guest}");
+    }
+    Ok(())
+}
