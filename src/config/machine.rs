4421746e8856f81888a824300d873b801064320f
Jeffrey Caffrey-Hill <jeff@caffreyhill.com>
2023-04-08 06:19:07 -0400

    bugfix: Fix machine interpolation

--- src/config/machine.rs
+++ src/config/machine.rs
@@ -17,13 +17,14 @@ impl AsArgs for MachineConfig {
   
     fn as_args(&self) -> Result<Vec<String>, Error> {
         if let Some(props) = &self.props {
-            let options = props.into_iter()
+            let mut options = props.into_iter()
                 .map(|(key, value)| format!("{key}={value}"))
-                .collect::<Vec<_>>()
-                .join(",");
+                .collect::<Vec<_>>();
+            options.insert(0, self.r#type.clone());
+            let options = options.join(",");
             Ok(vec![format!("-machine"), options])
         } else {
-            Ok(Vec::new())
+            Ok(vec![format!("-machine"), self.r#type.clone()])
         }
     }
 }
@@ -35,12 +36,28 @@ mod tests {
 
     #[test]
     fn args() {
+        let config = 
+            MachineConfig {
+                r#type: format!("virt"),
+                props: None
+            };
+        let args = config.as_args().unwrap();
+        assert_eq!(args.len(), 2);
+        assert_eq!(args[0], "-machine");
+        assert_eq!(args[1], "virt");
+    }
+
+    #[test]
+    fn args_with_props() {
         let mut config = 
             MachineConfig {
                 r#type: format!("virt"),
                 props: Some(HashMap::new())
             };
         config.props.as_mut().unwrap().insert(format!("highmem"), format!("on"));
-        assert_eq!(config.as_args().unwrap().len(), 2);
+        let args = config.as_args().unwrap();
+        assert_eq!(args.len(), 2);
+        assert_eq!(args[0], "-machine");
+        assert_eq!(args[1], "virt,highmem=on");
     }
 }
