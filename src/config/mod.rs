9aeae99955fbab9403767abc5a5104919e22f4f2
Jeffrey Caffrey-Hill <jeff@caffreyhill.com>
2023-04-08 06:20:24 -0400

    maint: Add newline for clean code

--- src/config/mod.rs
+++ src/config/mod.rs
@@ -38,6 +38,7 @@ pub struct GuestConfig {
 
     #[serde(skip_serializing_if = "Option::is_none")]
     accel: Option<String>,
+
     #[serde(skip_serializing_if = "Option::is_none")]
     machine: Option<MachineConfig>,
 
