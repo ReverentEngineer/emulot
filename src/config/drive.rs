cbab08347d8c1544a644413bf8ebb6718f97d592
Jeff Caffrey-Hill <jeff@caffreyhill.com>
2023-10-08 12:03:05 -0400

    Fix bug in DriveConfig

--- src/config/drive.rs
+++ src/config/drive.rs
@@ -4,51 +4,26 @@ use crate::{
     Error
 };
 use serde::{Serialize, Deserialize};
+use std::collections::HashMap;
 
 #[derive(Clone, Debug, Default, PartialEq, Deserialize, Serialize)]
 pub struct DriveConfig {
 
-    /// The interface of the drive
-    #[serde(skip_serializing_if = "Option::is_none")]
-    r#if: Option<String>,
-
-    /// The file backing the drive
-    #[serde(skip_serializing_if = "Option::is_none")]
-    file: Option<String>,
-
-    /// The format of the file backing the drive
-    #[serde(skip_serializing_if = "Option::is_none")]
-    format: Option<String>,
-
-    /// Media format (e.g. cdrom)
-    #[serde(skip_serializing_if = "Option::is_none")]
-    media: Option<String>
+    #[serde(flatten)]
+    options: HashMap<String, String>,
 }
 
 impl AsArgs for DriveConfig {
 
     fn as_args(&self) -> Result<Vec<String>, Error> {
 
-        let mut options = Vec::new();
+        let mut args = Vec::new();
+        args.extend(
+            self.options.iter()
+            .map(|(key, value)| format!("{key}={value}"))
+        );
 
-        if let Some(r#if) = &self.r#if {
-            options.push(format!("if={}", r#if));
-        }
-
-        if let Some(file) = &self.file {
-            options.push(format!("file={}", file));
-        }
-
-        if let Some(file) = &self.file {
-            options.push(format!("file={}", file));
-        }
-
-
-        if !options.is_empty() {
-            Ok(vec![String::from("-drive"), options.join(",")])
-        } else {
-            Ok(Vec::new())
-        }
+        Ok(vec![format!("-drive"), args.join(",")])
     }
 }
 
